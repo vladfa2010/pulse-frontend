cbdeb2f98bbc8c46111e7d90b98e3aa1c88dd344
vladfa2010 <vladfa@yandex.ru>
2026-05-26 00:37:05 +0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router'
+import { useAuth } from './hooks/useAuth'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Pricing from './pages/Pricing'
+import Profile from './pages/Profile'
+import NewsFeed from './pages/NewsFeed'
+import Admin from './pages/Admin'
+import Terms from './pages/Terms'
+import Privacy from './pages/Privacy'
+
+export default function App() {
+  const { isLoggedIn, user } = useAuth()
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/news" element={<NewsFeed />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/privacy" element={<Privacy />} />
+    </Routes>
+  )
+}
